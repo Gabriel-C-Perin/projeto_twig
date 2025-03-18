9ce362fad2667fbdfd43c914a6c810dcf06a7113
Gabriel-C-Perin <gabrielchesiniperin@gmail.com>
2025-03-18 08:50:13 -0300

    aula 3

--- compras.php
+++ compras.php
@@ -1,7 +1,14 @@
 <?php
 // compras.php
 require_once('twig_carregar.php');
+require('inc/banco.php');
+
+$dados = $pdo->query('SELECT * from compras');
+
+$comp = $dados->fetchAll(PDO::FETCH_ASSOC);
+print_r($comp);
 
 echo $twig->render('compras.html', [
     'titulo' => 'Compras',
+    'compras' => $comp,
 ]);
