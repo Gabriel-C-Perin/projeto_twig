d99d5e1155684fb1ed9b54c9274eca0a661572a7
Gabriel-C-Perin <gabrielchesiniperin@gmail.com>
2025-03-25 07:58:38 -0300

    aula 5

--- compras.php
+++ compras.php
@@ -3,10 +3,10 @@
 require_once('twig_carregar.php');
 require('inc/banco.php');
 
-$dados = $pdo->query('SELECT * from compras');
+// Busca as compras no banco
+$dados = $pdo->query('SELECT * FROM compras');
 
 $comp = $dados->fetchAll(PDO::FETCH_ASSOC);
-print_r($comp);
 
 echo $twig->render('compras.html', [
     'titulo' => 'Compras',
